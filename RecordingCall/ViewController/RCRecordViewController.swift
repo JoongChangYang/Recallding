58feb193a5f83be5ca72bcaa8172a9d2d0652c11
JoongChangYang <didwndckd@gmail.com>
2020-08-22 00:15:44 +0900

    ADD: RCRecortViewController 추가, FIX: RCMainViewController.swift -> RCContentListViewController.swift

---
+++ RecordingCall/ViewController/RCRecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RCRecordViewController.swift
+//  RecordingCall
+//
+//  Created by 양중창 on 2020/08/22.
+//  Copyright © 2020 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class RCRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
