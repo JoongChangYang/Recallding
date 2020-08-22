e64c3ff8b756da8a744a6d45b3c066eb51c063eb
JoongChangYang <didwndckd@gmail.com>
2020-08-23 02:57:37 +0900

    UPDATE: 녹음버튼 색상 추가

--- RecordingCall/Util/RCColor.swift
+++ RecordingCall/Util/RCColor.swift
@@ -11,4 +11,7 @@ import UIKit
 enum RCColor {
   static let background = UIColor.white
   static let controller = #colorLiteral(red: 0, green: 1, blue: 0.4088452483, alpha: 1)
+  static let recordButtonBorder = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+  static let startRecord = #colorLiteral(red: 1, green: 0.303696692, blue: 0.4154136777, alpha: 1)
+  static let pauseRecord = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
 }
